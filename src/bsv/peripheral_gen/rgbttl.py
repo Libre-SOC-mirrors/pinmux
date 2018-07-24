311cea904e5563dc5fc4325da802c0a89e542e0f
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2018-07-24 11:25:09 +0100

    add rgbttl mk_pincon

--- src/bsv/peripheral_gen/rgbttl.py
+++ src/bsv/peripheral_gen/rgbttl.py
@@ -17,4 +17,20 @@ class rgbttl(PBase):
         return "lcd{0}.slave"
 
     def pinname_out(self, pname):
-        return pname
+        if not pname.startswith('out'):
+            return pname
+        return ''
+
+    def mk_pincon(self, name, count):
+        ret = [PBase.mk_pincon(self, name, count)]
+        # special-case for gpio in, store in a temporary vector
+        plen = len(self.peripheral.pinspecs)
+        template = "      mkConnection({0}.{1},\n\t\t\t{2}.{1});"
+        name = self.get_iname(count)
+        ps = "pinmux.peripheral_side.%s" % name
+        n = "{0}".format(name)
+        for ptype in ['data_out']:
+            ret.append(template.format(ps, ptype, n))
+        return '\n'.join(ret)
+
+
