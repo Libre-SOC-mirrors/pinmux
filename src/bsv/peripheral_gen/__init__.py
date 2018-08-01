980ea40231eb1e2abb9eacbf0955d2dea83163f9
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2018-08-01 08:30:53 +0100

    remove unneeded imports

--- src/bsv/peripheral_gen/__init__.py
+++ src/bsv/peripheral_gen/__init__.py
@@ -1,13 +1,1 @@
 from bsv.peripheral_gen.base import PeripheralInterfaces, PeripheralIface
-from uart import uart
-from quart import quart
-from sdmmc import sdmmc
-from pwm import pwm
-from eint import eint
-from rs232 import rs232
-from twi import twi
-from eint import eint
-from jtag import jtag
-from spi import spi
-from qspi import qspi
-from gpio import gpio
