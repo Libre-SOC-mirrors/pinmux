cb19c42084d016573bdde30de8535123f245e9aa
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-04-24 21:46:45 +0100

    argh reorder functions to not be recursively dependent

--- src/pinmux_generator.py
+++ src/pinmux_generator.py
@@ -89,20 +89,20 @@ if __name__ == '__main__':
         with open(fname, "w") as of:
             with open(pyname, "w") as pyf:
                 ps = module.pinspec()
-                pm, chip = module.pinparse(ps, pinspec)
-                litexmap = ps.pywrite(pyf, pm)
                 pinout, bankspec, pin_spec, fixedpins = ps.write(of)
                 #chip['litex.map'] = litexmap
-                chip = json.dumps(chip)
-                with open("%s/litex_pinpads.json" % pinspec, "w") as f:
-                    f.write(chip)
-
                 if testing:
                     dummytest(ps, output_dir, output_type)
                 else:
                     specgen(of, output_dir, pinout,
                             bankspec, ps.muxwidths, pin_spec, fixedpins,
                             ps.fastbus)
+                pm, chip = module.pinparse(ps, pinspec)
+                litexmap = ps.pywrite(pyf, pm)
+                chip = json.dumps(chip)
+                with open("%s/litex_pinpads.json" % pinspec, "w") as f:
+                    f.write(chip)
+
     else:
         if output_type == 'bsv':
             from bsv.pinmux_generator import pinmuxgen as gentypes
