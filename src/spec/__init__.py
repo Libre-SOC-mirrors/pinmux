5701c5dd6dac444f59271053277cea8d3e143914
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-11-06 15:37:59 +0000

    add ngi_router spec

--- src/spec/__init__.py
+++ src/spec/__init__.py
@@ -4,6 +4,7 @@ from spec import i_class
 from spec import minitest
 from spec import microtest
 from spec import ls180
+from spec import ngi_router
 from spec.gen import specgen
 from spec.testing import dummytest
 
@@ -12,5 +13,6 @@ modules = {'m_class': m_class,
            'i_class': i_class,
            'minitest': minitest,
            'microtest': microtest,
-           'ls180': ls180
+           'ls180': ls180,
+           'ngi_router': ngi_router
            }
